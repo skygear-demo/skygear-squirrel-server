45b8b723e8aa05c6748e3b408658b160e13d951e
TY <tatgean@gmail.com>
2017-05-04 12:28:52 +0800

    Add a simple integration test

---
+++ test/integration-test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const expect = chai.expect;
+const GitHubRepo = require('../src/GitHubRepo');
+const requestResolver = require('../src/request-resolver');
+
+describe('Integration test', () => {
+	it('all components should work together', () => {
+		const gitHubRepo = new GitHubRepo('https://api.github.com/',
+			'https://github.com/tatgean/skygear-squirrel-endpoint-test', 
+			null
+		);
+		return requestResolver.resolve(gitHubRepo, '0.1.0', 'osx').then(result => {
+			expect(result).to.deep.equal({
+				statusCode: 200,
+				body: {
+					url: 'https://github.com/tatgean/skygear-squirrel-endpoint-test/releases/download/v1.0.0/testing-osx.zip'
+				}
+			})
+		})
+	})
+})
