90fc1e8807b7df533d1892a9f875bd1726392f86
TY <tatgean@gmail.com>
2017-05-04 16:39:26 +0800

    Fix strigify typo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ skygearCloud.handler('update', function (req, options) {
 	return requestResolver.resolve(githubRepo, req.query.version, req.query.platform).then(result => {
 		return new SkygearResponse({
 			statusCode: result.statusCode,
-			body: JSON.strigify(result.body)
+			body: JSON.stringify(result.body)
 		})
 	})
 }, {
