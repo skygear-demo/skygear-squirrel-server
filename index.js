f80d221529f514799c9023cf7629df65cecd4fc4
TY <tatgean@gmail.com>
2017-05-04 16:36:02 +0800

    Stringify response body

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ skygearCloud.handler('update', function (req, options) {
 	return requestResolver.resolve(githubRepo, req.query.version, req.query.platform).then(result => {
 		return new SkygearResponse({
 			statusCode: result.statusCode,
-			body: result.body
+			body: JSON.strigify(result.body)
 		})
 	})
 }, {
