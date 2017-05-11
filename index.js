7c0a4a0ffccf4217bc6c8431304390b1f55953ce
TY <tatgean@gmail.com>
2017-05-11 15:15:29 +0800

    Add GITHUB_REPO not set error message

--- index.js
+++ index.js
@@ -4,28 +4,33 @@ const skygearCloud = require('skygear/cloud');
 const SkygearResponse = skygearCloud.SkygearResponse;
 const requestResolver = require('./src/request-resolver');
 const GitHubRepo = require('./src/GitHubRepo');
-const githubRepo = new GitHubRepo('https://api.github.com/',
-	process.env.GITHUB_REPO, 
-	process.env.GITHUB_ACCESS_TOKEN
-	);
-let serverStatus = 'Initializing....'
 
-githubRepo.fetchReleases().then(function() {
-	serverStatus = 'GitHub repo connected!'
-	skygearCloud.handler('update', function (req, options) {
-		return requestResolver.resolve(githubRepo, req.query.version, req.query.platform).then(result => {
-			return new SkygearResponse({
-				statusCode: result.statusCode,
-				body: JSON.stringify(result.body)
+let serverStatus = 'Initializing....';
+try {
+	const githubRepo = new GitHubRepo('https://api.github.com/',
+		process.env.GITHUB_REPO, 
+		process.env.GITHUB_ACCESS_TOKEN
+	);
+	githubRepo.fetchReleases().then(function() {
+		serverStatus = 'GitHub repo connected!'
+		skygearCloud.handler('update', function (req, options) {
+			return requestResolver.resolve(githubRepo, req.query.version, req.query.platform).then(result => {
+				return new SkygearResponse({
+					statusCode: result.statusCode,
+					body: JSON.stringify(result.body)
+				})
 			})
+		}, {
+			method: ['GET', 'POST'],
+			userRequired: false
 		})
-	}, {
-		method: ['GET', 'POST'],
-		userRequired: false
+	}).catch(function() {
+		serverStatus = 'Connection with GitHub repo failed. Check your environment variables(GITHUB_REPO and GITHUB_ACCESS_TOKEN) in Skygear portal.'
 	})
-}).catch(function() {
-	serverStatus = 'Connection with GitHub repo failed. Check your environment variables(GITHUB_REPO and GITHUB_ACCESS_TOKEN) in Skygear portal.'
-})
+
+} catch (e) {
+	serverStatus = 'Environment variables GITHUB_REPO not set';
+}
 
 skygearCloud.handler('getStatus', function(req, options) {
 	return 'Server Status: ' + serverStatus;
