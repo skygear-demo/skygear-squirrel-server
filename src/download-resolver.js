4471c5e297ae3b6bbdfc98880b3453e4204e55d0
TY <tatgean@gmail.com>
2017-05-17 15:21:12 +0800

    Invalid platform to resolve

--- src/download-resolver.js
+++ src/download-resolver.js
@@ -1,12 +1,16 @@
+'use strict';
+
 const releaseParaser = require('./release-parser');
 const semver = require('semver');
-
+const acceptedPlatforms = ['osx', 'win'];
 function DownloadResolver(githubRepo) {
     this._githubRepo = githubRepo;
 }
 
 DownloadResolver.prototype.resolve = function(platform, version) {
     return this._githubRepo.fetchReleases().then(releases => {
+        if (acceptedPlatforms.indexOf(platform) === -1)
+            throw new Error('Invalid platform - ' + platform);
         for (let i = 0; i < releases.length; i++) {
             let r = releaseParaser.parse(releases[i]);
             if (semver.eq(r.version, version) && r[platform]) {
--- test/download-resolver-test.js
+++ test/download-resolver-test.js
@@ -57,5 +57,41 @@ describe('download-resolver', () => {
         })
     });
 
+    it('resolve() invalid platform', () => {
+        const assetId = 1;
+        server.on({
+            method: 'GET',
+            path: '/repos/some-user/some-repo/releases',
+            reply: {
+                status: 200,
+                body: JSON.stringify([
+                    {
+                        tag_name: 'v1.0.0',
+                        prerelease: false,
+                        assets: [
+                            {
+                                id: assetId,
+                                name: 'osx.zip'
+                            }
+                        ]
+                    }
+                ])
+            }
+        });
+        server.on({
+            method: 'GET',
+            path: '/repos/some-user/some-repo/releases/assets/' + assetId,
+            reply: {
+                status: 200,
+                body: 'some-data'
+            }
+        });
+        const host = 'http://localhost:9000/';
+        const repoUrl = 'some-user/some-repo';
+        const accessToken = null;
+        const githubRepo = new GitHubRepo(host, repoUrl, accessToken);
+        const resolver = new DownloadResolver(githubRepo);
+        return expect(resolver.resolve('an-invalid-platform', '1.0.0')).to.be.rejectedWith('Invalid platform - an-invalid-platform');
+    });
 
 });
